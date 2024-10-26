139d7eddc6eade9a8223447b3e42f13ce8ea800c
Justin Msaga <146983172+justinmsaga@users.noreply.github.com>
2024-10-26 16:41:55 -0700

    code refactoring and emojify

--- players.js
+++ players.js
@@ -17,8 +17,19 @@ class player {
   }
 
   autoSelect() {
-    const selection = ["fire", "rock", "paper", "scissors", "water"];
-    return selection[Math.floor(Math.random() * 5)];
+    const selection = ["🔥", "🪨", "🧻", "✂️", "🌊"];
+    const num = Math.floor(Math.random() * 100);
+    if (num < 33) {
+      return selection[0];
+    } else if (num < 44) {
+      return selection[1];
+    } else if (num < 55) {
+      return selection[2];
+    } else if (num < 66) {
+      return selection[3];
+    } else {
+      return selection[4];
+    }
   }
 }
 
